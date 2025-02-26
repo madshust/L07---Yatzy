a7de381b7c6580e7d20e2c84aff8de2df242e380
Victor Møller <160784673+Loenskov79@users.noreply.github.com>
2025-02-26 13:40:09 +0100

    kinda works

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { rollDice } from "./controller.js";
-import { showDice } from "./ui.js";
+import { lockDice, updateDice } from "./ui.js";
 
 
 const rollButton = document.getElementById("rollButton")
@@ -19,8 +19,22 @@ rollButton.addEventListener("click", function () {
 
     diceArray = rollDice(diceArray);
 
-    showDice(diceArray);
+    updateDice(diceArray);
     counter++;
-
+    document.getElementById("counter").innerText = `Turn ${counter}`;
 }
 )
+
+const images = document.querySelectorAll('img');
+
+
+for (let img of images) {
+    img.addEventListener("click", function() {
+
+        lockDice()
+
+    })
+}
+
+
+
