3bb95be0e553a272f20081ca4f10a8fea08ca293
Loui Forsingdal <eaa24lfn@students.eaaa.dk>
2025-02-26 12:18:42 +0100

    rules

--- controller.js
+++ controller.js
@@ -1,11 +1,23 @@
 
+let values = [];
+
+export function getValues()
+{
+    return values
+}
+
 export function rollDice() {
 
-    let result = [];
-
     for (let i = 0; i < 5; i++) {
-        result[i] = Math.floor(Math.random() * 6) + 1
+        values[i] = Math.floor(Math.random() * 6) + 1
     }
-
-    return result;
+    getValues
 }
+
+
+
+
+
+
+
+
